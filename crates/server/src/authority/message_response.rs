3bc2b2ef92a545608e1ef2eb45596548b01841dd
Andy Bao <contact@andybao.me>
2022-10-13 15:56:20 +0000

    Expose some of MessageResponseBuilder

--- crates/server/src/authority/message_response.rs
+++ crates/server/src/authority/message_response.rs
@@ -128,7 +128,7 @@ impl<'q> MessageResponseBuilder<'q> {
     /// # Arguments
     ///
     /// * `query` - any optional query (from the Request) to associate with the Response
-    pub(crate) fn new(query: Option<&'q WireQuery>) -> MessageResponseBuilder<'q> {
+    pub fn new(query: Option<&'q WireQuery>) -> MessageResponseBuilder<'q> {
         MessageResponseBuilder {
             query,
             sig0: None,
